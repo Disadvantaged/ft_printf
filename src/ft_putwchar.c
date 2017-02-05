1e03aa172cc7122d75de5d77f992a80e27800dc0
Dimitris GOLEAR <dgolear@e1p67.chisinau.42.fr>
2017-02-05 13:17:11 +0200

    changed structure of folders

--- src/ft_putwchar.c
+++ src/ft_putwchar.c
@@ -1,31 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*   ft_putwchar.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dgolear <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/10/15 08:29:26 by dgolear           #+#    #+#             */
-/*   Updated: 2016/10/23 16:42:02 by dgolear          ###   ########.fr       */
+/*   Created: 2017/01/07 13:57:53 by dgolear           #+#    #+#             */
+/*   Updated: 2017/01/25 16:23:04 by dgolear          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_strncmp(const char *s1, const char *s2, size_t n)
+int		ft_putwchar(wchar_t c)
 {
-	size_t	i;
-
-	i = 0;
-	while (i < n)
-	{
-		if (*s1 != *s2)
-			return (*(unsigned char*)s1 - *(unsigned char*)s2);
-		if (*s1 == '\0' && *s2 == '\0')
-			return (0);
-		s1++;
-		s2++;
-		i++;
-	}
-	return (0);
+	return (ft_putwchar_fd(c, 1));
 }
